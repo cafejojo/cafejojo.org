abe022a427e8a87ea4c481f2bcecb35fdda6762f
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2018-05-31 18:01:00 +0200

    Add improved 404 page

--- src/pages/schaapi.js
+++ src/pages/schaapi.js
@@ -6,7 +6,7 @@ const SchaapiPage = () => (
         <p className="leading-loose text-xl">🐑 Safe Changes for APIs</p>
 
         <div className="mt-8">
-            <a className="btn btn-green inline-block no-underline" href="https://github.com/cafejojo/schaapi">
+            <a className="btn btn-green inline-block" href="https://github.com/cafejojo/schaapi">
                 <i className="fa fa-github mr-2" />
                 github.com/cafejojo/schaapi
             </a>
